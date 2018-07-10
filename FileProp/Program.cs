9c18645766088a2c4e41b9e729494354c48fdffb
claushun <claushun@ix.co.za>
2018-07-10 08:46:34 +0200

    Add project files.

---
+++ FileProp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileProp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+      string path = @"C:\Users\Miguel\Desktop\MyTest.csv";
+
+      // Set the variable "delimiter" to ", ".
+      string delimiter = ", ";
+
+      DateTime creationTime = File.GetCreationTime(@"C:\Users\Miguel\Desktop\Importer\Importer");
+      DateTime lastWriteTime = File.GetLastWriteTime(@"C:\Users\Miguel\Desktop\Importer\Importer");
+      DateTime lastAccessTime = File.GetLastAccessTime(@"C:\Users\Miguel\Desktop\Importer\Importer");
+
+      string[] array = Directory.GetFiles(@"C:\Users\Miguel\Desktop\Importer\Importer"); // store files within a directory into a list
+                                                                                         //string[] entries = Directory.GetFileSystemEntries(path, "*", SearchOption.AllDirectories);
+
+      if (!File.Exists(path))
+      {
+        string createtext = creationTime + delimiter + lastWriteTime + delimiter + lastAccessTime + delimiter + Environment.NewLine;
+        File.WriteAllText(path, createtext);
+      }
+
+
+      // Keep the console window open in debug mode.
+      string readtext = File.ReadAllText(path);
+
+      Console.WriteLine("Press any key to exit.");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ FileProp/Test2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileProp
+{
+  class FileInfo
+  {
+    static void prop()
+    {
+
+      string path = @"C:\Users\Miguel\Desktop\Importer\Importer";
+      string[] entries = Directory.GetFileSystemEntries(path, "*", SearchOption.AllDirectories);
+      string delimiter = ", ";
+
+
+      DateTime creationTime = File.GetCreationTime(@"C:\Users\Miguel\Desktop\Importer\Importer");
+      DateTime lastWriteTime = File.GetLastWriteTime(@"C:\Users\Miguel\Desktop\Importer\Importer");
+      DateTime lastAccessTime = File.GetLastAccessTime(@"C:\Users\Miguel\Desktop\Importer\Importer");
+
+      foreach (string f in entries)
+      {
+        string createtext = creationTime + delimiter + lastWriteTime + delimiter + lastAccessTime + delimiter + Environment.NewLine;
+        File.WriteAllText(path, createtext);
+      }
+
+    }
+  }
+}
